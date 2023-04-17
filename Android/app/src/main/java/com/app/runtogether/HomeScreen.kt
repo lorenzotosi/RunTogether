d238744f634ccf07e081db2ce0ad103c320555d7
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-04-17 10:48:57 +0200

    fix sizes to show bot bars

--- Android/app/src/main/java/com/app/runtogether/HomeScreen.kt
+++ Android/app/src/main/java/com/app/runtogether/HomeScreen.kt
@@ -1,10 +1,15 @@
 package com.app.runtogether
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
@@ -18,9 +23,9 @@ fun ShowHomeScreen(navController : NavHostController = rememberNavController()){
         NavigationGraph(navController, pValues)
 
         var selectedItem by remember { mutableStateOf(0) }
-        val items = listOf("Songs", "Artists", "Playlists")
+        val items = listOf("Run", "Today's Runs", "Challenges")
 
-        NavigationBar {
+        NavigationBar (modifier = Modifier.padding(vertical = 64.dp)) {
             items.forEachIndexed { index, item ->
                 NavigationBarItem(
                     icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
@@ -34,9 +39,14 @@ fun ShowHomeScreen(navController : NavHostController = rememberNavController()){
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GenerateTopBar(){
+    CenterAlignedTopAppBar(
+        title = { Text(text = "Run") },
+        
 
+    )
 }
 
 @Composable
