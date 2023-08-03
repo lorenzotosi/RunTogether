b270fcc48d138d7e5013b30947f4ed6071c913d6
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-08-03 10:33:22 +0200

    add map

--- Android/app/src/main/java/com/app/runtogether/RunScreen.kt
+++ Android/app/src/main/java/com/app/runtogether/RunScreen.kt
@@ -1,8 +1,28 @@
 package com.app.runtogether
 
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun ShowRunScreen(){
-    //todo
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            position = singapore,
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
 }
