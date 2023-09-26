ae564bfc925cc5ca059ebbcc36bc7be016b4c406
Andrea Zavatta <zavattaandrea@gmail.com>
2023-09-26 11:23:10 +0200

    fix some bugs

--- Android/app/src/main/java/com/app/runtogether/SessionManager.kt
+++ Android/app/src/main/java/com/app/runtogether/SessionManager.kt
@@ -1,53 +1,65 @@
-package com.app.runtogether
-
 import android.content.Context
 import android.content.SharedPreferences
-import com.app.runtogether.db.user.User
 
-
-class SessionManager(private val _context: Context) {
+class SessionManager private constructor(context: Context) {
     // Shared Preferences
+    private val pref: SharedPreferences
+    private val editor: SharedPreferences.Editor
+    private val PRIVATE_MODE = 0
 
-    private val pref: SharedPreferences = _context.getSharedPreferences(PREF_NAME, 0)
-    private val editor: SharedPreferences.Editor = pref.edit()
+    // Sharedpref file name
+    private val PREF_NAME = "AndroidHivePref"
+
+    // All Shared Preferences Keys
+    private val IS_LOGIN = "IsLoggedIn"
+    val KEY_NAME = "name"
+    val KEY_EMAIL = "email"
+    val KEY_PASSWORD = "password"
+
+    init {
+        pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE)
+        editor = pref.edit()
+    }
+
     companion object {
-        private const val PREF_NAME = "AndroidHivePref"
-        private const val IS_LOGIN = "IsLoggedIn"
-        const val KEY_NAME = "name"
-        const val KEY_EMAIL = "email"
-        const val KEY_PASSWORD = "password"
+        @Volatile
+        private var INSTANCE: SessionManager? = null
 
-        fun createLoginSession(context: Context, username: String, email: String, password: String) {
-            val pref = context.getSharedPreferences(PREF_NAME, 0)
-            val editor = pref.edit()
-            editor.putBoolean(IS_LOGIN, true)
-            editor.putString(KEY_NAME, username)
-            editor.putString(KEY_EMAIL, email)
-            editor.putString(KEY_PASSWORD, password)
-            editor.apply()
+        private fun getInstance(context: Context): SessionManager {
+            return INSTANCE ?: synchronized(this) {
+                val instance = SessionManager(context)
+                INSTANCE = instance
+                instance
+            }
         }
 
-        fun getUserDetails(context: Context): User{
-            return User(
-                username = context.getSharedPreferences(PREF_NAME, 0).getString(KEY_NAME, "").toString(),
-                email = context.getSharedPreferences(PREF_NAME, 0).getString(KEY_EMAIL, "").toString(),
-                password = context.getSharedPreferences(PREF_NAME, 0).getString(KEY_PASSWORD, "").toString()
-            )
+        fun createLoginSession(context: Context, username: String, email: String, password: String) {
+            val sessionManager = getInstance(context)
+            sessionManager.editor.putBoolean(sessionManager.IS_LOGIN, true)
+            sessionManager.editor.putString(sessionManager.KEY_NAME, username)
+            sessionManager.editor.putString(sessionManager.KEY_EMAIL, email)
+            sessionManager.editor.putString(sessionManager.KEY_PASSWORD, password)
+            sessionManager.editor.apply()
+        }
+
+        fun getUserDetails(context: Context): HashMap<String, String> {
+            val sessionManager = getInstance(context)
+            val user = HashMap<String, String>()
+            user[sessionManager.KEY_NAME] = sessionManager.pref.getString(sessionManager.KEY_NAME, null) ?: ""
+            user[sessionManager.KEY_EMAIL] = sessionManager.pref.getString(sessionManager.KEY_EMAIL, null) ?: ""
+            return user
         }
 
         fun isLoggedIn(context: Context): Boolean {
-            val pref = context.getSharedPreferences(PREF_NAME, 0)
-            return pref.getBoolean(IS_LOGIN, false)
+            val sessionManager = getInstance(context)
+            return sessionManager.pref.getBoolean(sessionManager.IS_LOGIN, false)
         }
 
         fun logoutUser(context: Context) {
-            val pref = context.getSharedPreferences(PREF_NAME, 0)
-            val editor = pref.edit()
-            editor.clear()
-            editor.apply()
+            val sessionManager = getInstance(context)
+            sessionManager.editor.clear()
+            sessionManager.editor.apply()
         }
-
-
-
     }
+
 }
