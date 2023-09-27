bd8f024666468d8c27dbf021a979f9db6bb1d22d
Andrea Zavatta <zavattaandrea@gmail.com>
2023-09-27 09:59:47 +0200

    add creator id to Run table

--- Android/app/src/main/java/com/app/runtogether/db/run/Run.kt
+++ Android/app/src/main/java/com/app/runtogether/db/run/Run.kt
@@ -2,14 +2,25 @@ package com.app.runtogether.db.run
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import com.app.runtogether.db.user.User
 import java.util.*
 
-@Entity
+@Entity(
+    foreignKeys = [
+        ForeignKey(
+            entity = User::class,
+            parentColumns = ["id"],
+            childColumns = ["user_creator_id"],
+            onDelete = ForeignKey.CASCADE
+        )]
+)
 data class Run(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     @ColumnInfo(name = "date") val name: Date?,
     @ColumnInfo(name = "description") val description: String?,
     @ColumnInfo(name = "city") val path: String?,
-    @ColumnInfo(name = "lengthKm") val lengthKm: Int?
+    @ColumnInfo(name = "lengthKm") val lengthKm: Int?,
+    val user_creator_id: Int
 )
