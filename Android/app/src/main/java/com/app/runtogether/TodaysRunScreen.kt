4a194a3ed4ba48c3ef82a744170c602ecf9620b7
lorenzotosi <93598576+lorenzotosi@users.noreply.github.com>
2023-10-16 00:48:54 +0200

    new buttons

--- Android/app/src/main/java/com/app/runtogether/TodaysRunScreen.kt
+++ Android/app/src/main/java/com/app/runtogether/TodaysRunScreen.kt
@@ -3,20 +3,26 @@ package com.app.runtogether
 import SessionManager
 import android.location.Geocoder
 import android.util.Log
+import androidx.annotation.ColorRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -176,6 +182,119 @@ fun CardRun(navController: NavHostController, location : LocationDetails){
                     .background(MaterialTheme.colorScheme.secondaryContainer)
                     .padding(8.dp)
             )
+        }
+    }
+}
+
+
+@Composable
+fun SegmentedControl(
+    items: List<String>,
+    defaultSelectedItemIndex: Int = 0,
+    useFixedWidth: Boolean = false,
+    itemWidth: Dp = 120.dp,
+    cornerRadius : Int = 10,
+    @ColorRes color : Int = R.color.teal_200,
+    onItemSelection: (selectedItemIndex: Int) -> Unit
+) {
+    val selectedIndex = remember { mutableStateOf(defaultSelectedItemIndex) }
+
+    Row(
+        modifier = Modifier
+    ) {
+        items.forEachIndexed { index, item ->
+            OutlinedButton(
+                modifier = when (index) {
+                    0 -> {
+                        if (useFixedWidth) {
+                            Modifier
+                                .width(itemWidth)
+                                .offset(0.dp, 0.dp)
+                                .zIndex(if (selectedIndex.value == index) 1f else 0f)
+                        } else {
+                            Modifier
+                                .wrapContentSize()
+                                .offset(0.dp, 0.dp)
+                                .zIndex(if (selectedIndex.value == index) 1f else 0f)
+                        }
+                    } else -> {
+                        if (useFixedWidth)
+                            Modifier
+                                .width(itemWidth)
+                                .offset((-1 * index).dp, 0.dp)
+                                .zIndex(if (selectedIndex.value == index) 1f else 0f)
+                        else Modifier
+                            .wrapContentSize()
+                            .offset((-1 * index).dp, 0.dp)
+                            .zIndex(if (selectedIndex.value == index) 1f else 0f)
+                    }
+                },
+                onClick = {
+                    selectedIndex.value = index
+                    onItemSelection(selectedIndex.value)
+                },
+                shape = when (index) {
+                    /**
+                     * left outer button
+                     */
+                    0 -> RoundedCornerShape(
+                        topStartPercent = cornerRadius,
+                        topEndPercent = 0,
+                        bottomStartPercent = cornerRadius,
+                        bottomEndPercent = 0
+                    )
+                    /**
+                     * right outer button
+                     */
+                    items.size - 1 -> RoundedCornerShape(
+                        topStartPercent = 0,
+                        topEndPercent = cornerRadius,
+                        bottomStartPercent = 0,
+                        bottomEndPercent = cornerRadius
+                    )
+                    /**
+                     * middle button
+                     */
+                    else -> RoundedCornerShape(
+                        topStartPercent = 0,
+                        topEndPercent = 0,
+                        bottomStartPercent = 0,
+                        bottomEndPercent = 0
+                    )
+                },
+                border = BorderStroke(
+                    1.dp, if (selectedIndex.value == index) {
+                        colorResource(id = color)
+                    } else {
+                        colorResource(id = color).copy(alpha = 0.75f)
+                    }
+                ),
+                colors = if (selectedIndex.value == index) {
+                    /**
+                     * selected colors
+                     */
+                    ButtonDefaults.outlinedButtonColors(
+                        containerColor = colorResource(
+                            id = color
+                        )
+                    )
+                } else {
+                    /**
+                     * not selected colors
+                     */
+                    ButtonDefaults.outlinedButtonColors(containerColor = Color.Transparent)
+                },
+            ) {
+                Text(
+                    text = item,
+                    fontWeight = FontWeight.Normal,
+                    color = if (selectedIndex.value == index) {
+                        Color.White
+                    } else {
+                        colorResource(id = color).copy(alpha = 0.9f)
+                    },
+                )
+            }
         }
     }
 }
